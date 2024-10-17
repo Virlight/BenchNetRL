8ce16e17678ebbdb3f209bc74ca24f0e8b997e50
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-10-17 13:32:09 +0200

    Add shell scripts for classic control

---
+++ scripts/ours/classic_control.sh
@@ -0,0 +1,12 @@
+# CartPole-v1
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id CartPole-v1 --track --seed 1 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id CartPole-v1 --track --seed 2 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id CartPole-v1 --track --seed 3 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+# Acrobot-v1
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id Acrobot-v1 --track --seed 1 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id Acrobot-v1 --track --seed 2 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id Acrobot-v1 --track --seed 3 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+# MountainCar-v0
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id MountainCar-v0 --track --seed 1 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id MountainCar-v0 --track --seed 2 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
+OMP_NUM_THREADS=1 python ppo_mamba.py --gym-id MountainCar-v0 --track --seed 3 --wandb-project-name ppo-mamba --cuda True --total-timesteps 500000
