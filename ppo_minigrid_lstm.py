ee3482af707967cf72b7a514edef692a7c14414f
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-11-25 22:13:18 +0100

    Change arguments

--- ppo_minigrid_lstm.py
+++ ppo_minigrid_lstm.py
@@ -29,7 +29,7 @@ class RemoveMissionWrapper(gym.ObservationWrapper):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", type=str, default="ppo_minigrid_lstm",
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
         help="the name of this experiment")
     parser.add_argument("--gym-id", type=str, default="MiniGrid-Empty-8x8-v0",
         help="the id of the gym environment")
@@ -45,7 +45,7 @@ def parse_args():
         help="if toggled, cuda will be enabled by default")
     parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="if toggled, this experiment will be tracked with Weights and Biases")
-    parser.add_argument("--wandb-project-name", type=str, default="ppo-minigrid-lstm",
+    parser.add_argument("--wandb-project-name", type=str, default="ppo-minigrid",
         help="the wandb's project name")
     parser.add_argument("--wandb-entity", type=str, default=None,
         help="the entity (team) of wandb's project")
@@ -53,7 +53,7 @@ def parse_args():
         help="whether to capture videos of the agent performances (check out `videos` folder)")
 
     # Algorithm specific arguments
-    parser.add_argument("--num-envs", type=int, default=8,
+    parser.add_argument("--num-envs", type=int, default=1,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
         help="the number of steps to run in each environment per policy rollout")
--- ppo_minigrid_mamba.py
+++ ppo_minigrid_mamba.py
@@ -34,7 +34,7 @@ class RemoveMissionWrapper(gym.ObservationWrapper):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", type=str, default="PPO_MiniGrid_Mamba",
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py"),
         help="the name of this experiment")
     parser.add_argument("--gym-id", type=str, default="MiniGrid-Empty-8x8-v0",
         help="the id of the gym environment")
@@ -50,7 +50,7 @@ def parse_args():
         help="if toggled, cuda will be enabled by default")
     parser.add_argument("--track", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="if toggled, this experiment will be tracked with Weights and Biases")
-    parser.add_argument("--wandb-project-name", type=str, default="ppo-minigrid-mamba",
+    parser.add_argument("--wandb-project-name", type=str, default="ppo-minigrid",
         help="the wandb's project name")
     parser.add_argument("--wandb-entity", type=str, default=None,
         help="the entity (team) of wandb's project")
